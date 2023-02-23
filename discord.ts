6ccdbf3ffab3dbd89fe5f6957479e780f6976a91
jami3f <jamie.fairhurst01@gmail.com>
2023-02-23 18:38:17 +0000

    Added discord bot

---
+++ discord.ts
@@ -0,0 +1,38 @@
+import "https://deno.land/std@0.177.0/dotenv/load.ts";
+
+import {
+  Intents,
+  Client,
+  MessageAttachment,
+} from "https://deno.land/x/harmony@v2.8.0/mod.ts";
+
+import { Status, checkGlasses } from "./scraper.ts";
+
+const client = new Client({
+  intents: Intents.None,
+});
+
+client.on("ready", async () => {
+  const glassesName = "grayson";
+  const in_stock = (await checkGlasses(glassesName)) == Status.in_stock;
+
+  const stockMessage = in_stock
+    ? "in stock <@626438519065149460>"
+    : "out of stock";
+  const statusMessage = `${glassesName} is ${stockMessage}`;
+
+  const channels = client.channels;
+
+  const image = await MessageAttachment.load("./grayson_page.png");
+
+  const testChannelID = "1078382288565444630";
+  const importantChannelID = "1078377336266817606";
+  const channelToUse = in_stock ? importantChannelID : testChannelID;
+
+  await channels.sendMessage(channelToUse, statusMessage);
+  await channels.sendMessage(channelToUse, { file: image });
+
+  Deno.exit();
+});
+
+client.connect();
--- scraper.ts
+++ scraper.ts
@@ -1,19 +1,33 @@
 import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
 
-await page.goto(
-  "https://www.glassesdirect.co.uk/glasses/grayson/?prca=PRCA50039128&aspect=front"
-);
-
-await page.waitForSelector("div#product-actions");
-const outOfStockDiv = await page.$("div#product-out-of-stock");
-const outOfStockClass = await outOfStockDiv?.getProperty("className");
-const inStock = (await outOfStockClass?.jsonValue()).includes("hidden");
-if (inStock) {
-  console.log("In stock");
-} else {
-  console.log("Out of stock");
+export enum Status {
+  in_stock,
+  out_of_stock,
 }
 
-await browser.close();
+export async function checkGlasses(glassesName: string) {
+  const browser = await puppeteer.launch({
+    defaultViewport: { width: 1920, height: 1080, deviceScaleFactor: 1 },
+  });
+  const page = await browser.newPage();
+
+  await page.goto(
+    `https://www.glassesdirect.co.uk/glasses/${glassesName}/?prca=PRCA50039128&aspect=front`
+  );
+
+  await page.waitForSelector("div#product-actions");
+  const outOfStockDiv = await page.$("div#product-out-of-stock");
+  const outOfStockClass = await outOfStockDiv?.getProperty("className");
+  const inStock = (await outOfStockClass?.jsonValue()).includes("hidden");
+  let status;
+  if (inStock) {
+    status = Status.in_stock;
+  } else {
+    status = Status.out_of_stock;
+  }
+
+  await page.screenshot({ path: "grayson_page.png" });
+
+  await browser.close();
+  return status;
+}
